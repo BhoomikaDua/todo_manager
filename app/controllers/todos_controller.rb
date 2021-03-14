5357df843d7be6fbea19279f74b8e58e29f62617
BhoomikaDua <dua.bhoomika@gail.com>
2021-03-14 21:03:31 +0530

    Dynamic Design

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,7 +3,8 @@ class TodosController < ApplicationController
   protect_from_forgery with: :null_session
 
   def index
-    render plain: Todo.order(:due_date).map{ |todo| todo.to_pleasant_string}.join("\n")
+    #render plain: Todo.order(:due_date).map{ |todo| todo.to_pleasant_string}.join("\n")
+    render "index.html.erb"
   end
 
   def show
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,23 @@ class Todo < ActiveRecord::Base
     completed_icon = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_s(:long)} #{todo_text} #{completed_icon}"
   end
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
 end
