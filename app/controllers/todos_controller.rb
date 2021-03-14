776cbd5af586e48e5f60e1de37f7d59ac607923a
BhoomikaDua <dua.bhoomika@gail.com>
2021-03-14 14:13:31 +0530

    Added Create new todo and update todo functionality Bug Fix

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,7 @@
 # todos_controller.rb
 class TodosController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
     render plain: Todo.order(:due_date).map{ |todo| todo.to_pleasant_string}.join("\n")
   end
@@ -28,6 +30,9 @@ class TodosController < ApplicationController
     todo = Todo.find(id)
     todo.completed = completed
     todo.save
+
+    response_text = "Hey, your todo with the id #{todo.id} is updated"
+    render plain: response_text
   end
 
 end
