f185cf0439330ec9de5aea4ea0029a23bce78648
BhoomikaDua <dua.bhoomika@gail.com>
2021-03-14 10:45:54 +0530

    Formatted todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+  def to_pleasant_string
+    completed_icon = completed ? "[X]" : "[ ]"
+    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{completed_icon}"
+  end
 end
